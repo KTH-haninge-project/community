29b8a78fb8f67f389552d29539e743e71ed45bb0
luben93 <luben93@gmail.com>
2014-10-15 11:25:39 +0200

    3 empty classes

---
+++ Community/Community/Models/MesssagesViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Community.Models
+{
+    public class MesssagesViewModel
+    {
+        public MesssagesViewModel() 
+        { 
+        //empty constructor 
+        }
+
+    }
+}
