fc9bc837a81ae1fea2638178630afd4c454e9e7a
luben93 <luben93@gmail.com>
2014-10-21 13:41:16 +0200

    felsöker default controller

--- Community/Community/Controllers/DefaultController.cs
+++ Community/Community/Controllers/DefaultController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Data.Entity;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using Community.Models;
@@ -14,21 +16,22 @@ namespace Community.Controllers
 {
     public class DefaultController:Controller
     {
-        public DefaultController()
+        public DefaultController():base()
         {
             ApplicationDbContext db = new ApplicationDbContext();
-            try{
+            ViewBag.unread = "not logedins";
+            //try{
                 if( User.Identity.IsAuthenticated){
                     String id = User.Identity.GetUserId();
                     List<ReadEntry> unreadlist = db.ReadEntries.Where(r => r.Receiver.Equals(id)).ToList();
 
-                    ViewBag.unread = unreadlist.Count();
+                    ViewBag.unread = "["+unreadlist.Count()+"]";
                     String outputer = ViewBag.unread + "";
                     Debug.WriteLine("tot messages: " + outputer);
                 }
-            }catch (NullReferenceException e){
-            
-            }
+           // }catch (NullReferenceException e){
+           //    String t= e.Message;
+           // }
         }
     }
 }
